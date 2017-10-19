7df2384c9c8d2f1075c5a3d8d42698536e377d54
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2017-10-18 17:00:54 -0700

    pruebas al thunkSubject

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import * as rx from "rxjs";
+
+type ThunkState = "valid" | "invalid" | "invalidAsync";
 /**A subject that execute the given thunk on the first subscription */
 class ThunkSubjectClass<T>
     extends rx.BehaviorSubject<T | Promise<T>>
@@ -8,55 +10,82 @@ class ThunkSubjectClass<T>
         this.thunk = thunk;
     }
     private thunk: () => Promise<T>;
-    private firstCall:boolean = true;
-
+    private state: ThunkState = "invalid";
+    /**Version de la ultima invalidación del cache */
+    private invalidAsyncVersion: number = 0;
 
     protected _subscribe(o: rx.Subscriber<T | Promise<T>>) {
         //Force the first value to be getted
-        this.lastValue();
+        this.refreshValidation(true);
         return super._subscribe(o);
     }
 
-    next(value: T | Promise<T>) {
-        super.next(value);
+
+    /**Refresh the subject value with a promise value if there are any subscriptors*/
+    invalidate() {
+        this.state = "invalid";
+        this.refreshValidation(false);
     }
 
-    /**Refresh the subject value with a promise value*/
-    refresh() {
-        super.next(this.thunk());
+    async invalidateAsync() {
+        this.state = "invalidAsync";
+        await this.refreshValidation(false);
     }
 
-    /**Gets the last value of the thunk subject */
-    lastValue() : T | Promise<T> {
-        if (this.firstCall) {
-            const value = this.thunk();
-            this.next(value);
-            this.firstCall = false;
-            return value;
-        } else {
-            return this.value;
+    /**
+     * Call the validation methods 
+     * @param force Call even if there aren't any observers
+     */
+    private async refreshValidation(force: boolean) {
+        if (this.observers.length > 0 || force) {
+            switch(this.state) {
+                case "invalid":
+                    this.forceValidateSync();
+                    break;
+                case "invalidAsync":
+                    await this.forceValidateAsync();
+                    break;
+            }
         }
     }
 
-    /**Refresh the subject value with a non- */
-    async refreshAsync() {
-        super.next(await this.thunk());
+    private forceValidateSync() {
+        const value = this.thunk();
+        this.next(value);
+        this.state = "valid";
     }
+
+    private async forceValidateAsync() {
+        const version = Math.random();
+        this.invalidAsyncVersion = version;
+        const value = await this.thunk();
+
+        //checamos que la version del cache invalidado continue siendo la misma
+        if (this.invalidAsyncVersion == version) {
+            this.next(value);
+            this.state = "valid";
+        }
+    }
+
 }
 
 /**
 /**A subject that execute the given thunk on the first subscription 
  */
 export interface ThunkSubject<T> extends rx.Observable<T | Promise<T>> {
-    /**Refresh the subject value with a promise value, the observers will receive the promise as is*/
-    refresh();
-    /**Refresh the subject value with a non-promise value, the observers will receive the resolved value */
-    refreshAsync(): Promise<void>;
+    /**If there are any subscriptions, evaluate the thunk immediatly and pass the promise value to the subscribers, 
+     * if there isn't any subscriptions, mark the subject as invalid and executes the thunk on the next subscription.
+     * Subscribers will receive the promise of the value */
+    invalidate();
+    /**If there are any subscriptions, evaluate the thunk, await the result pass the promise value to the subscribers, 
+     * if there isn't any subscriptions, mark the subject as invalidAsync and executes then awaits the thunk on the next subscription.
+     * Subscribers will receive the solved value */
+    invalidateAsync(): Promise<void>;
 }
 
 /**
 /**Crate a subject that executes the given thunk at the first subscription and on refresh calls
  */
-export function thunkSubject<T>(thunk : () => Promise<T>) : ThunkSubject<T> {
+export function thunkSubject<T>(thunk: () => Promise<T>): ThunkSubject<T> {
     return new ThunkSubjectClass<T>(thunk);
 }
--- src/test.ts
+++ src/test.ts
@@ -1,59 +1,150 @@
 import { thunkSubject } from "./index";
 import * as expect from "expect";
+import * as rx from "rxjs";
+function delay(ms: number) {
+    return new Promise<number>(resolve => {
+        setTimeout(() => {
+            resolve(counter);
+        }, ms);
+    });
+}
 
-let counter = 1;
+let counter = 0;
 let llamadas = 0;
 async function testThunk() {
     llamadas++;
     return new Promise<number>(resolve => {
         setTimeout(() => {
-            resolve(counter);
             counter++;
+            resolve(counter);
         }, 100);
     });
 }
 
 (async () => {
+    console.log("Probar que testThunk sólo sea llamado cuando existan subscriptores vigentes");
     const subject = thunkSubject(testThunk);
     //Antes de llamar al testThunk no se ha aumentado el counter
     expect(llamadas).toBe(0);
 
+    //Si llamamos al refresh no se llama al testThunk, ya que no hay ningun subscriptor
+    subject.invalidate();
+    expect(llamadas).toBe(0);
+
     //La primera llamada
     {
-        const counter = await await new Promise<Promise<number> | number>(resolve => subject.subscribe(resolve));
+        let subs: rx.Subscription = null as any;
+        const counter = await await new Promise<Promise<number> | number>(resolve => subs = subject.subscribe(resolve));
         expect(counter).toBe(1);
         expect(llamadas).toBe(1);
+        expect(counter).toBe(llamadas);
+        subs.unsubscribe();
     }
 
 
     //Ya no genera mas llamadas
     {
-        const counter = await await new Promise<Promise<number> | number>(resolve => subject.subscribe(resolve));
+        let subs: rx.Subscription = null as any;
+        const counter = await await new Promise<Promise<number> | number>(resolve => subs = subject.subscribe(resolve));
         expect(counter).toBe(1);
         expect(llamadas).toBe(1);
+        expect(counter).toBe(llamadas);
+        subs.unsubscribe();
     }
 
     //creamos 2 subscriptores:
     let subA: number | Promise<number> = 0;
     let subB: number | Promise<number> = 0;
 
-    subject.subscribe(x => subA = x);
-    subject.subscribe(x => subB = x);
+    const subscriptionA = subject.subscribe(x => subA = x);
+    const subscriptionB = subject.subscribe(x => subB = x);
 
     //Obtienen el valor anterior, que es la promesa con el 1:
     expect(await subA).toBe(1);
     expect(await subB).toBe(1);
+    expect(counter).toBe(llamadas);
 
     //actualizamos el valor del subject con un valor que no es una promesa
-    await subject.refreshAsync();
+    await subject.invalidateAsync();
+    expect(counter).toBe(llamadas);
 
     expect(subA).toBe(2);
     expect(subB).toBe(2);
 
     //actualizamos el valor del subject con uno que es una promesa:
-    subject.refresh();
+    subject.invalidate();
     expect(await subA).toBe(3);
     expect(await subB).toBe(3);
+    expect(counter).toBe(llamadas);
+
+    //Eliminamos la subscriptionB
+    subscriptionB.unsubscribe();
+
+    //Actualizamos el valor de subject, sólo debe de actualizar subA
+    await subject.invalidateAsync();
+    expect(counter).toBe(llamadas);
+
+    expect(await subA).toBe(4);
+    expect(await subB).toBe(3);
+    expect(counter).toBe(llamadas);
+
+    //Eliminamos la subscripcionA
+    subscriptionA.unsubscribe();
+    expect(counter).toBe(llamadas);
+
+    //Agregamos de nuevo la subscripcionA
+    let subC: number | Promise<number> = 0;
+    const subscriptionC = subject.subscribe(x => subC = x);
+
+    //Debe de agarrar el valor anterior:
+    expect(subC).toBe(4);
+    expect(counter).toBe(llamadas);
+
+    //Invalidamos el valor
+    await subject.invalidateAsync();
+    expect(counter).toBe(llamadas);
+
+    //Se regeneró el valor
+    expect(subC).toBe(5);
+    expect(counter).toBe(llamadas);
+
+    //Si quitamos la subscriocion no regenera el valor:
+    subscriptionC.unsubscribe();
+
+    expect(llamadas).toBe(5);
+    subject.invalidate();
+    //No realiza ninguna llamada ya que no hay subscriptores:
+    expect(llamadas).toBe(5);
+
+    //Agregamos un subscriptor, el cual va a generar una llamada ya que se marco como invalido el valor
+    let subD: number | Promise<number> = 0;
+    const subscriptionD = subject.subscribe(x => subD = x);
+    //Al tener el nuevo subscriptor recibimos la llamada
+    expect(llamadas).toBe(6);
+    expect(await subD).toBe(6);
+
+    subscriptionD.unsubscribe();
+
+    //Realizamos el invalidado asincronom sin subscriptores, lo que ocasionará que no se realize la llamada hasta que se subcriba uno, pero el siguiente subscriptor tendra el valor tal cual, no la promesa sin resolver
+    expect(llamadas).toBe(6);
+    await subject.invalidateAsync();
+    //No realiza ninguna llamada ya que no hay subscriptores:
+    expect(llamadas).toBe(6);
+
+    let subF: number | Promise<number> = 0;
+    const subscriptionF = subject.subscribe(x => subF = x);
+    //Al entrar el subscriptor realizó la llamada
+    expect(llamadas).toBe(7);
+    //Al subscriptor se le dio el valor anterior, ya que el nuevo valor aún se esta calculando
+    expect(await subF).toBe(6);
+
+    //esperamos a que termine 
+    await delay(150);
+    //Ahora si el subF tiene el valor mas reciente, y no es una promesa si no el valor tal cual
+    expect(subF).toBe(7);
 
     console.log("all test passed");
-})();
+})().then(x => {
+}, x => {
+    console.warn(x);
+});
