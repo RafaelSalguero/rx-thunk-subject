c0383538b606d5ef1ca2d4c68d27c7af7b6096b1
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2017-12-08 10:20:54 -0700

    exports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { ThunkSubjectClassMemo } from "./memo";
 import { ThunkSubjectClassNoMemo } from "./nomemo";
 import { ThunkSubjectMemo, ThunkSubject } from "./types";
 
+export { ThunkSubject, ThunkSubjectMemo };
+
 /**
 /**Crate a subject that executes the given thunk at the first subscription and on invalidate calls
  */
@@ -11,6 +13,6 @@ export function thunkSubjectMemo<T>(thunk: () => Promise<T>): ThunkSubjectMemo<T
 }
 
 /**Create a new subject that always executes the given thunk, on all subscriptions and on invalidate calls */
-export function thunkSubject<T>(thunk: () => Promise<T>) : ThunkSubject<T> {
+export function thunkSubject<T>(thunk: () => Promise<T>): ThunkSubject<T> {
     return new ThunkSubjectClassNoMemo<T>(thunk);
 }
